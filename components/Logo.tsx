cdedd786c419d7087bc268483e061847d2c2a8e9
h-zahar <hz.rifat@gmail.com>
2023-11-10 08:01:23 +0600

    Fixed Logo Sizing

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ import LogoImage from "@/images/logos/babello.svg";
 function Logo() {
   return (
     <Link href="/" prefetch={false} className="overflow-hidden">
-      <div className="flex items-center w-28 h-14">
+      <div className="flex items-center w-40 sm:w-28 h-14">
         <AspectRatio
           ratio={16 / 9}
           className="flex items-center justify-center"
