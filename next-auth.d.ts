03647c87009014c12549c0b5818d86292f21ab6a
h-zahar <hz.rifat@gmail.com>
2023-11-11 07:16:52 +0600

    Reconfigured Type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,7 +2,7 @@ import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    firebaseToke?: string;
+    firebaseToken?: string;
     user: {
       id: string;
     } & DefaultSession["user"];
