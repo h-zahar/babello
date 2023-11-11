820d64940bcbe47f985e1f9f78d07cdd684cb223
h-zahar <hz.rifat@gmail.com>
2023-11-11 06:24:13 +0600

    Implemented Id Fetching

--- auth.ts
+++ auth.ts
@@ -10,6 +10,22 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
+        }
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+  },
   session: {
     strategy: "jwt",
   },
