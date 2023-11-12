5a0095413578fc2a53518cf8fdfb636caf2a8efc
h-zahar <hz.rifat@gmail.com>
2023-11-13 05:49:02 +0600

    Implemented Stripe Checkout Portal

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { db } from "@/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 
@@ -21,6 +21,19 @@ function CheckoutButton() {
         cancel_url: window.location.origin,
       }
     );
+    return onSnapshot(docRef, (snap) => {
+      const data = snap.data();
+      const url = data?.url;
+      const error = data?.error;
+
+      if (error) {
+        alert("Soemthing went wrong!");
+        setLoading(false);
+      } else if (url) {
+        window.location.assign(url);
+        setLoading(false);
+      }
+    });
   };
 
   return (
@@ -29,7 +42,7 @@ function CheckoutButton() {
         className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
         onClick={() => createCheckoutSession()}
       >
-        Sign Up
+        {loading ? "Loading..." : "Sign Up"}
       </button>
     </div>
   );
