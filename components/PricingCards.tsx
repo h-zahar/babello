c12a2413a76c9d1b4904ae8c2951fd566b2f5a5d
h-zahar <hz.rifat@gmail.com>
2023-11-13 06:45:19 +0600

    Customized Pricing View

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -44,53 +44,63 @@ function PricingCards({
   ];
   return (
     <div>
-      <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
+      <div
+        className={`mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl${
+          !isLogged && " lg:grid-cols-2"
+        }`}
+      >
         {tiers.map((tier, i) => {
           return (
             <div
               key={i}
               className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
             >
-              <div>
-                <h3 className="text-base font-semibold leading-7 text-indigo-600">
-                  {tier.name}
-                </h3>
-                <div className="mt-4 flex items-baseline gap-x-2">
-                  {tier.priceMonthly ? (
-                    <>
+              {(isLogged && tier?.id) || !isLogged ? (
+                <div>
+                  <h3 className="text-base font-semibold leading-7 text-indigo-600">
+                    {tier.name}
+                  </h3>
+                  <div className="mt-4 flex items-baseline gap-x-2">
+                    {tier.priceMonthly ? (
+                      <>
+                        <span className="text-5xl font-bold tracking-tight text-gray-900">
+                          {tier.priceMonthly}
+                        </span>
+                        <span className="text-base font-semibold leading-7 text-gray-700">
+                          /month
+                        </span>
+                      </>
+                    ) : (
                       <span className="text-5xl font-bold tracking-tight text-gray-900">
-                        {tier.priceMonthly}
+                        Free
                       </span>
-                      <span className="text-base font-semibold leading-7 text-gray-700">
-                        /month
-                      </span>
-                    </>
-                  ) : (
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">
-                      Free
-                    </span>
-                  )}
+                    )}
+                  </div>
+                  <p className="mt-6 text-base leading-7 text-gray-600">
+                    {tier.description}
+                  </p>
+                  <ul
+                    role="list"
+                    className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+                  >
+                    {tier.features.map((feature, i) => {
+                      return (
+                        <li key={i} className="flex gap-x-3">
+                          <CheckIcon
+                            className="h-6 flex-none text-indigo-600"
+                            aria-hidden="true"
+                          />
+                          {feature}
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
-                <p className="mt-6 text-base leading-7 text-gray-600">
-                  {tier.description}
+              ) : (
+                <p className="text-indigo-600 text-center">
+                  <span className="text-black">Welcome to</span> Babello!
                 </p>
-                <ul
-                  role="list"
-                  className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
-                >
-                  {tier.features.map((feature, i) => {
-                    return (
-                      <li key={i} className="flex gap-x-3">
-                        <CheckIcon
-                          className="h-6 flex-none text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        {feature}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+              )}
               {redirect && !tier?.id && !isLogged && (
                 <Link
                   href="/register"
