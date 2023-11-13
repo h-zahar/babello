e099cceb4ff4df37fd31a94887810a85c159bd29
h-zahar <hz.rifat@gmail.com>
2023-11-13 10:03:05 +0600

    Integrated with Stripe Portal

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -16,7 +16,7 @@ function CheckoutButton() {
     const docRef = await addDoc(
       collection(db, "customers", session.user.id, "checkout_sessions"),
       {
-        price: process.env.STRIPE_PRO_ID,
+        price: "price_1OBjfnGwl7hhS0i3nh173iyE",
         success_url: window.location.origin,
         cancel_url: window.location.origin,
       }
