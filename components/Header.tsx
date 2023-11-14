0303746c71e850b4ad46d3644d7c7349c3ef829e
h-zahar <hz.rifat@gmail.com>
2023-11-14 18:35:09 +0600

    Inserted Upgrade Banner to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { authOptions } from "@/auth";
 import Link from "next/link";
 import { MessagesSquareIcon } from "lucide-react";
 import CreateChatButton from "./CreateChatButton";
+import UpgradeBanner from "./UpgradeBanner";
 
 async function Header() {
   const session = await getServerSession(authOptions);
@@ -35,6 +36,8 @@ async function Header() {
           <UserButton session={session} />
         </div>
       </nav>
+
+      <UpgradeBanner />
     </header>
   );
 }
