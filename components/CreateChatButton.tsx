669b4b0ce25c0c0dfc1e95c2d2637726539a7f2c
h-zahar <hz.rifat@gmail.com>
2023-11-16 18:49:56 +0600

    Implemented Chat Creation

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,15 +1,59 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { MessageSquarePlusIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useSubscriptionStore } from "@/store/store";
+import { useToast } from "./ui/use-toast";
+import { v4 as uuidv4 } from "uuid";
+import { addChatRef } from "@/lib/converters/chatMembers";
+import { serverTimestamp, setDoc } from "firebase/firestore";
 
-function CreateChatButton() {
+function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
+  const { data: session } = useSession();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  const subscription = useSubscriptionStore((state) => state.subscription);
 
   const createNewChat = async () => {
-    router.push(`/chat/ch`);
+    if (!session?.user.id) return;
+    setLoading(true);
+    toast({
+      title: "Creating new chat...",
+      description: "Just a bit...",
+      duration: 3000,
+    });
+
+    const chatId = uuidv4();
+
+    await setDoc(addChatRef(chatId, session.user.id), {
+      userId: session.user.id,
+      email: session.user.email!,
+      timestamp: serverTimestamp(),
+      isAdmin: true,
+      chatId: chatId,
+      image: session.user.image || "",
+    })
+      .then(() => {
+        toast({
+          title: "Success",
+          description: "Your chat has been created!",
+          className: "bg-green text-white",
+          duration: 2000,
+        });
+        router.push(`/chat/${chatId}`);
+      })
+      .catch((error) => {
+        toast({
+          title: "Error",
+          description: "Something went wrong",
+          variant: "destructive",
+        });
+      })
+      .finally(() => setLoading(false));
   };
 
   return (
