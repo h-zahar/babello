8cdfd129013414008d0f0b42c86e0a7bbb448e7b
h-zahar <hz.rifat@gmail.com>
2023-11-20 23:45:15 +0600

    Included Chat Rows

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,15 +1,22 @@
 import { authOptions } from "@/auth";
+import { chatMemberCollectionGroupRef } from "@/lib/converters/chatMembers";
 import { getDocs } from "firebase/firestore";
 import { getServerSession } from "next-auth";
 import React from "react";
+import ChatListRows from "./ChatListRows";
 
 async function ChatList() {
   const session = await getServerSession(authOptions);
 
-  // const chatsSnapshot = await getDocs(
+  const chatsSnapshot = await getDocs(
+    chatMemberCollectionGroupRef(session?.user.id!)
+  );
+  const initialChats = chatsSnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    timestamp: null,
+  }));
 
-  // );
-  return <div>ChatList</div>;
+  return <ChatListRows initialChats={initialChats} />;
 }
 
 export default ChatList;
