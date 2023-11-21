25d6d251b00f0e8ff8f32def245fafa351574f5a
h-zahar <hz.rifat@gmail.com>
2023-11-21 23:30:18 +0600

    Added Chat Input to UI

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,7 +1,22 @@
+import { authOptions } from "@/auth";
+import ChatInput from "@/components/ChatInput";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-function Chat() {
-  return <div>Chat</div>;
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+async function Chat({ params: { chatId } }: Props) {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <>
+      <ChatInput chatId={chatId} />
+    </>
+  );
 }
 
 export default Chat;
