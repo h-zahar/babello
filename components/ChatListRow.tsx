e98d9ded81955c0fb46a6383a082a8538a91fb43
h-zahar <hz.rifat@gmail.com>
2023-11-21 23:27:52 +0600

    Implemented Rows UI

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -4,10 +4,55 @@ import { Message, limitedMessagesRef } from "@/lib/converters/message";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
+import UserAvatar from "./UserAvatar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function ChatListRow({ chatId }: { chatId: string }) {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   const [messages, loading, error] = useCollectionData<Message>(
     limitedMessagesRef(chatId)
+  );
+
+  const prettyUUID = (n = 4) => {
+    return chatId.substring(0, n);
+  };
+
+  const row = (message?: Message) => (
+    <div
+      key={chatId}
+      onClick={() => router.push(`chat/${chatId}`)}
+      className="flex p-5 items-center space-x-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-700"
+    >
+      <UserAvatar
+        name={message?.user.name! || session?.user.name!}
+        image={message?.user.image! || session?.user.image!}
+        className="border border-indigo-500 scale-125"
+      />
+
+      <div className="flex-1 ps-2">
+        <p className="font-bold">
+          {!message && "New Chat"}
+          {message &&
+            [message?.user.name || session?.user.name].toString().split(" ")[0]}
+        </p>
+
+        <p className="text-grey-400 line-clamp-1">
+          {message?.translated?.["en"] || "Get the conversation started..."}
+        </p>
+      </div>
+
+      <div className="text-xs text-grey-400">
+        <p className="mb-auto">
+          {message
+            ? new Date(message.timestamp).toLocaleTimeString()
+            : "No messages yet"}
+        </p>
+        <p>Chat #{prettyUUID()}</p>
+      </div>
+    </div>
   );
 
   return (
@@ -21,6 +66,9 @@ function ChatListRow({ chatId }: { chatId: string }) {
           </div>
         </div>
       )}
+
+      {messages?.length === 0 && !loading && row()}
+      {messages?.map((message) => row(message))}
     </div>
   );
 }
