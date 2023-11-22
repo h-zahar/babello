7e25877d4580fac50582d49798bcc39f6cb755af
h-zahar <hz.rifat@gmail.com>
2023-11-23 04:09:58 +0600

    Implemented Invite Feature

---
+++ components/InviteUser.tsx
@@ -0,0 +1,185 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+import { addChatRef, chatMembersRef } from "@/lib/converters/chatMembers";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { getUserByEmailRef } from "@/lib/converters/user";
+import { useToast } from "./ui/use-toast";
+import useAdminId from "@/hooks/useAdminId";
+import { PlusCircleIcon } from "lucide-react";
+// import { ShareLink } from "./ShareLink"
+import { useSubscriptionStore } from "@/store/store";
+import { ToastAction } from "./ui/toast";
+import { useRouter } from "next/navigation";
+import { Input } from "./ui/input";
+import ShareLink from "./ShareLink";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+});
+
+function InviteUser({ chatId }: { chatId: string }) {
+  const { data: session } = useSession();
+  const { toast } = useToast();
+  const adminId = useAdminId({ chatId });
+  const subscription = useSubscriptionStore((state) => state.subscription);
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [openInviteLink, setOpenInviteLink] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (!session?.user.id) return;
+
+    toast({
+      title: "Sending invite",
+      description: "Plase wait while we send the invite...",
+    });
+
+    const noOfUsersInChat = (await getDocs(chatMembersRef(chatId))).docs.map(
+      (doc) => doc.data()
+    ).length;
+
+    const isPro = subscription?.role === "pro";
+
+    if (!isPro && noOfUsersInChat >= 2) {
+      toast({
+        title: "Free plan limit exceeded",
+        description:
+          "You have exceeded the limit of users in a single chat for the fee plan. Please upgrade to PRO to enjoy unlimited invites.",
+        variant: "destructive",
+        action: (
+          <ToastAction
+            altText="upgrade"
+            onClick={() => router.push("/register")}
+          >
+            Upgrade to PRO
+          </ToastAction>
+        ),
+      });
+      return;
+    }
+
+    const querySnapShot = await getDocs(getUserByEmailRef(values.email));
+
+    if (querySnapShot.empty) {
+      toast({
+        title: "User not found",
+        description:
+          "Please enter an email of a registered user or resend the invitation once they have signed up!",
+        variant: "destructive",
+      });
+      return;
+    } else {
+      const user = querySnapShot.docs[0].data();
+      await setDoc(addChatRef(chatId, user.id), {
+        userId: user.id,
+        email: user.email!,
+        timestamp: serverTimestamp(),
+        chatId: chatId,
+        isAdmin: false,
+        image: user?.image || "",
+      }).then(() => {
+        setOpen(false);
+
+        toast({
+          title: "Added to chat!",
+          description: "User has been added to the chat successfully!",
+          className: "bg-green-600 text-white",
+          duration: 3000,
+        });
+        setOpenInviteLink(true);
+      });
+    }
+
+    form.reset();
+  };
+
+  return (
+    adminId === session?.user.id && (
+      <>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircleIcon className="mr-1" />
+              Add User
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle className="mb-2 mt-3">Add User to Chat</DialogTitle>
+              <DialogDescription>
+                Simply enter another users email address to invite them to this
+                chat{" "}
+                <span className="text-indigo-600">
+                  (They must be registered)
+                </span>
+              </DialogDescription>
+            </DialogHeader>
+
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col space-y-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="jason@doe.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="ml-auto sm:w-fit w-full pt-2">
+                  <Button type="submit">Add to Chat</Button>
+                </div>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
+
+        <ShareLink
+          chatId={chatId}
+          isOpen={openInviteLink}
+          setIsOpen={setOpenInviteLink}
+        />
+      </>
+    )
+  );
+}
+
+export default InviteUser;
---
+++ components/ShareLink.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { Dispatch, SetStateAction } from "react";
+
+import { Copy } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { useToast } from "./ui/use-toast";
+
+function ShareLink({
+  isOpen,
+  chatId,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  chatId: string;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) {
+  const { toast } = useToast();
+  const host = window.location.host;
+  const linkToChat =
+    process.env.NODE_ENV === "development"
+      ? `http://${host}/chat/${chatId}`
+      : `https://${host}/chat/${chatId}`;
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(linkToChat);
+
+      toast({
+        title: "Copied Successfully",
+        description:
+          "Share this to the person you want to chat with (They must be added to the chat to access it!)",
+        className: "bg-green-600 text-white",
+        duration: 3000,
+      });
+      setIsOpen(false);
+    } catch (error) {
+      console.log("Failed to copy the text");
+      setIsOpen(false);
+    }
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Copy className="mr-2" />
+          Share Link
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share Link</DialogTitle>
+          <DialogDescription>
+            Any user who has been{" "}
+            <span className="text-indigo-600 font-bold">granted access</span>{" "}
+            can use this
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input id="Link" defaultValue={linkToChat} readOnly />
+          </div>
+          <Button
+            type="submit"
+            onClick={() => copyToClipboard()}
+            size="sm"
+            className="px-3"
+          >
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+
+        {/* <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter> */}
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ShareLink;
