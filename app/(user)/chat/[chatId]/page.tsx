86e98433ec8986069e6150d99c73ddd97ff467df
h-zahar <hz.rifat@gmail.com>
2023-11-23 04:16:22 +0600

    Integrated Control Features

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,10 +1,13 @@
 import { authOptions } from "@/auth";
+import AdminControls from "@/components/AdminControls";
 import ChatInput from "@/components/ChatInput";
 import ChatMembersBadges from "@/components/ChatMembersBadges";
 import ChatMessages from "@/components/ChatMessages";
+import { chatMembersRef } from "@/lib/converters/chatMembers";
 import { sortedMessagesRef } from "@/lib/converters/message";
 import { getDocs } from "firebase/firestore";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
@@ -20,9 +23,18 @@ async function Chat({ params: { chatId } }: Props) {
     (doc) => doc.data()
   );
 
+  const hasAccess = (await getDocs(chatMembersRef(chatId))).docs
+    .map((doc) => doc.id)
+    .includes(session?.user.id!);
+
+  if (!hasAccess) {
+    redirect("/chat?error=permission");
+  }
+
   return (
     <>
       <div className="min-h-[calc(100vh-10rem)]">
+        <AdminControls chatId={chatId} />
         <ChatMembersBadges chatId={chatId} />
         <ChatMessages
           chatId={chatId}
